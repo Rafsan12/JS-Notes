995d2b6df3eb3393aaeb56e55ded231ed65e56b1
Rafsan12 <rafsan.web.2022@gmail.com>
2023-12-21 09:29:17 +0600

    introduction

---
+++ Introduction.js
@@ -0,0 +1,18 @@
+/*
+1.JavaScript was invented by Brendan Eich in 1995, and became an ECMA standard in 1997.
+
+2.ECMA-262 is the official name of the standard. ECMAScript is the official name of the language.
+
+3. Why Study JavaScript?
+
+JavaScript is one of the 3 languages all web developers must learn:
+
+   1. HTML to define the content of web pages
+
+   2. CSS to specify the layout of web pages
+
+   3. JavaScript to program the behavior of web pages
+
+4. Using JS you can change HTML Attribute Values, HTML elements, CSS Styles.
+
+*/
