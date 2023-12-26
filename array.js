2cb87626b9396d9d07dabb8668c316c039ef8d18
Rafsan12 <rafsan.web.2022@gmail.com>
2023-12-26 20:16:09 +0600

    array

--- array.js
+++ array.js
@@ -76,4 +76,81 @@ const name = names.find((_, index) => index % 2 == 0)
 
 const numbers10 =  [1,5,11,20,25,10,11,120];
 const firstrNumberGreater10 = numbers10.find(numbers => numbers > 10);
-console.log(firstrNumberGreater10);
+// console.log(firstrNumberGreater10);
+
+const objects = [
+    { name: "Alice", age: 25 },
+    { name: "Bob", age: 30 },
+    { name: "Charlie", age: 35 }
+  ];
+  const firstWithNameProperty = objects.find(object => object.hasOwnProperty("name")); // Returns any of the objects
+  // console.log(firstWithNameProperty)
+
+
+  /*
+  Array splice()
+
+Purpose:
+
+Modifies an array by removing or replacing existing elements and/or adding new elements in place.
+It directly changes the original array, unlike methods like filter or map that create new arrays.
+
+
+Here's information about the splice() method in JavaScript:
+
+Purpose:
+
+Modifies an array by removing or replacing existing elements and/or adding new elements in place.
+It directly changes the original array, unlike methods like filter or map that create new arrays.
+
+Syntax:
+array.splice(start, deleteCount, item1, item2, ...)
+
+Parameters:
+start: The index at which to start modifying the array.
+deleteCount (optional): The number of elements to remove. Defaults to 0.
+item1, item2, ... (optional): New elements to insert at the start index.
+
+
+Returns:
+An array containing the removed elements.
+
+Key Points:
+splice() directly modifies the original array.
+It can be used to remove, insert, or replace elements.
+It returns an array of the removed elements.
+Use it when you need to make direct changes to an existing array.
+
+  */
+
+
+/*
+Array slice()
+
+Purpose:
+Creates a new array containing a selected portion of an existing array, without modifying the original array.
+It's often used to extract, copy, or rearrange elements within arrays.
+Syntax:
+
+JavaScript
+array.slice(start, end)
+
+
+Parameters:
+start: The starting index of the slice (inclusive). Defaults to 0.
+end (optional): The ending index of the slice (exclusive). Defaults to the array's length.
+
+
+Returns:
+A new array containing the extracted elements.
+
+
+Key Points:
+slice() creates a new array, leaving the original array intact.
+It's a non-mutating method, unlike splice() which directly modifies the array.
+It's useful for extracting, copying, and rearranging array elements without affecting the source.
+It's often used in conjunction with other array methods like map, filter, and reduce to create new arrays with transformed data.
+
+
+*/
+  
