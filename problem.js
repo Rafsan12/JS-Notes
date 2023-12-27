2a64f9436ed41eed5574f652055c2876ccdd96aa
Rafsan12 <rafsan.web.2022@gmail.com>
2023-12-27 22:26:44 +0600

    problem

--- problem.js
+++ problem.js
@@ -87,3 +87,25 @@ const friends = ["Rafsan", "Raj", "Ahmed","RRRRRR"];
 const bestFriendName = bestFriend(friends);
 
 console.log(bestFriendName); 
+
+// এইটা একটু ট্রিকি হতে পারে। একটা array এর মধ্যে অনেকগুলা সংখ্যা থাকবে। তোমার কাজ হচ্ছে সংখ্যা গুলা একটার পর একটা করে চেক করা। এবং সংখ্যা গুলা যদি পজিটিভ সংখ্যা হয়। অর্থাৎ শূন্য বা শূন্যের চাইতে বড় হয় তাহলে সেগুলাকে কোন একটা array এর মধ্যে রাখবে। আর যদি নেগেটিভ সংখ্যা হয়। তাহলে লুপটা স্টপ করে দিবে। এবং লুপ বন্ধ করার আগ পর্যন্ত যতগুলা পজিটিভ সংখ্যা পাওয়া গেছে। সেগুলা রিটার্ন করে দিবে।
+
+function positiveNumbers(numbers)
+{
+    const positiveNumbers = [];
+    for(const number of numbers)
+    {
+        if(number >= 0)
+        {
+            positiveNumbers.push(number);
+        }
+        else{
+            break;
+        }
+    }
+    return positiveNumbers;
+}
+
+const numbers = [1,0,30, -7,4,0,-9];
+const findPositiveNumber = positiveNumbers(numbers);
+console.log(findPositiveNumber);
